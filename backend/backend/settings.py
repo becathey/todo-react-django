f89fa6746ea1afbf0db1758fb51fec2692d56f3d
Bill Cathey <billcathey@hotmail.com>
2022-02-13 23:08:36 -0500

    Correct middleware to corsheaders

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheader.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
